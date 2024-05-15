e1628ccde47b5a7998e49f91b774ddfe7c4f4ab6
Hart Woolery <gohart@gmail.com>
2024-05-15 10:35:33 -0700

    fixes for index page

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,68 @@
-import { bootstrapCameraKit, createMediaStreamSource } from '@snap/camera-kit'
+import { Count, LensSource, PartialContainer, bootstrapCameraKit, createMediaStreamSource } from '@snap/camera-kit'
+import { Push2Web } from "@snap/push2web";
+import { Subject } from 'rxjs';
+
+declare global {
+  interface Window {
+    onLogin: any;
+  }
+}
+
+function setDebug(text:string) {
+  let button = document.getElementById("logger")
+    if (button) {
+      button.innerHTML = text
+    }
+}
 
 (async function () {
-  const cameraKit = await bootstrapCameraKit({
-    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzEzODEwMjI2LCJzdWIiOiJlMGRkMGUxYy0xMTkyLTRlZjctODAwNi00YmExNmE0NTY2ZTV-U1RBR0lOR34wZGIxMjQ1ZC1mODU1LTQ0ZmItOGY5ZS03NGJkMjg0ZmQyYzAifQ.OSyXHIw-VFwTkiyebO93KHhOCb7JRSlTiEXn_FU0FTE',
+  const push2Web = new Push2Web();
+  var subscribed = false
+  window.onLogin = function(loginToken : string){
+    if (subscribed) return;
+    setDebug("Logged In")
+    console.log(loginToken)
+    localStorage.setItem("access_token", loginToken)
+
+    push2Web.subscribe(loginToken, session, cameraKit.lensRepository);
+
+    subscribed = true
+  }
+
+  push2Web.events.addEventListener("lensReceived", console.log);
+  push2Web.events.addEventListener("error", function(err:any) {
+    console.log(err.detail)
+    localStorage.removeItem("access_token")
+    setDebug("Logged Out")
   });
+  push2Web.events.addEventListener("subscriptionChanged", console.log);
+
+  const extensions = (container: { provides: (arg0: PartialContainer<{ lensSources: LensSource[]; externalMetricsSubject: Subject<Count>; }, {}>) => any; }) => container.provides(push2Web.extension);
+
+  var config : any = {
+    apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzEzODEwMjI2LCJzdWIiOiJlMGRkMGUxYy0xMTkyLTRlZjctODAwNi00YmExNmE0NTY2ZTV-U1RBR0lOR34wZGIxMjQ1ZC1mODU1LTQ0ZmItOGY5ZS03NGJkMjg0ZmQyYzAifQ.OSyXHIw-VFwTkiyebO93KHhOCb7JRSlTiEXn_FU0FTE'
+  }
+
+  // config.lensCoreOverrideUrls = {
+  //   wasm: 'https://snap-ck-ms-teams.s3.amazonaws.com/lc/LensCoreWebAssembly.wasm',
+  //   js: 'https://snap-ck-ms-teams.s3.amazonaws.com/lc/LensCoreWebAssembly.js',
+  // }
+
+  const cameraKit = await bootstrapCameraKit(config, extensions);
+
   const liveRenderTarget = document.getElementById(
-    'canvas'
+    'canvas1'
   ) as HTMLCanvasElement;
   const session = await cameraKit.createSession({ liveRenderTarget });
   const stream = await navigator.mediaDevices.getUserMedia({
     video: {
-      facingMode: "environment",  // This attempts to select the rear-facing camera
-      width:9999
+      facingMode: "environment"  // This attempts to select the rear-facing camera
     }
   });
 
 
   
-  //, width:  
-  const source = createMediaStreamSource(stream, { fpsLimit: 60 });
+  const source = createMediaStreamSource(stream, { fpsLimit: 30 });
 
 
   await session.setSource(source);
@@ -26,19 +70,41 @@ import { bootstrapCameraKit, createMediaStreamSource } from '@snap/camera-kit'
 
   await session.play();
 
+  let usePush = window.location.hostname == "awe.ngrok.app";
+  
+  if (usePush) {
+    
 
-  var lens = await cameraKit.lensRepository.loadLens(
-    '268dd9cd-5211-4658-8008-ab2db3762f46',
-    '6f0b1073-8317-460d-945e-9a389d66ca91'
-  );
+    const searchParams = new URLSearchParams(window.location.search);
+    let token = searchParams.get('access_token') || localStorage.getItem("access_token"); 
+    if (token) {
+      window.onLogin(token)
+    } 
 
-  lens.cameraFacingPreference = 2; //back camera
-
-  await session.applyLens(lens);
-
-  //let live = session.output.live;
-  //let ratio = live.width/live.height;
-  await source.setRenderSize(liveRenderTarget.width , liveRenderTarget.height);
+    
+    
+  } else {
+    var lens = await cameraKit.lensRepository.loadLens(
+      '268dd9cd-5211-4658-8008-ab2db3762f46',
+      '6f0b1073-8317-460d-945e-9a389d66ca91'
+    );
+  
+    lens.cameraFacingPreference = 2; //back camera
+    
+    await session.applyLens(lens);
+  }
+  let live = session.output.live;
+  let ratio = live.width/live.height;
+  let wWindow = document.documentElement.clientWidth
+  let h = document.documentElement.clientHeight
+  let w = h * ratio
+  liveRenderTarget.style.width = w + "px";
+  liveRenderTarget.style.height = h + "px";
+  liveRenderTarget.style.left = (wWindow - w) * 0.5 + "px"
+  //console.log(liveRenderTarget.width , liveRenderTarget.height)
+  //await source.setRenderSize(liveRenderTarget.width , liveRenderTarget.height);
   //console.log(window.innerWidth * ratio, window.innerHeight)
-})();
+})()
+
+
 
